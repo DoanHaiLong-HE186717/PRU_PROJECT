ca6494c46972f22881a9ea1b8f4a169eee276318
Long Doan <longdhhe186717@fpt.edu.vn>
2025-02-05 09:40:55 +0700

    First Weapon

---
+++ Assets/Kawaii Survivor/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Elements")]
+    [SerializeField]
+    private Player players;
+
+
+
+    [Header("Settings")]
+    [SerializeField]
+    private float moveSpeed;
+
+    // Update is called once per frame
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (players != null)
+        {
+            FollowPlayer();
+        }
+
+    }
+
+    public void StorePlayer(Player player) => this.players = player;
+
+
+    private void FollowPlayer()
+    {
+        Vector2 targetPosition = Vector2.MoveTowards(transform.position, players.transform.position, moveSpeed * Time.deltaTime);
+
+        transform.position = targetPosition;
+    }
+}
---
+++ Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,132 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    enum State
+    {
+        Idle,
+        Attack
+    }
+    private State state;
+
+    [Header("Elements")]
+    [SerializeField]
+    private Transform hitDetectionTransform;
+    [SerializeField]
+    private float hitDetectionRadius;
+
+    [Header("Settings")]
+    [SerializeField]
+    private float range;
+    [SerializeField]
+    private LayerMask enemyMask;
+
+    [Header("Attack")]
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private Animator animator;
+    private List<Enemy> damagedEnemies = new List<Enemy>();
+
+
+    [Header("Animation")]
+    [SerializeField]
+    private float aimLerp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = State.Idle;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                AutoAim();
+                break;
+            case State.Attack:
+                Attacking();
+                break;
+        }
+
+
+    }
+    private void AutoAim()
+    {
+        Enemy closestEnemy = GetClosestEnemy();
+
+        Vector2 targetUpVector = Vector3.up;
+
+        if (closestEnemy != null)
+        {
+            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        }
+        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+
+    }
+
+    [NaughtyAttributes.Button]
+    private void StartAttack()
+    {
+        animator.Play("Attack");
+        state = State.Attack;
+    }
+    private void Attacking()
+    {
+        Attack();
+    }
+
+    private void StopAttack()
+    {
+        state = State.Idle;
+    }
+    private void Attack()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask);
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+    private Enemy GetClosestEnemy()
+    {
+        Enemy closestEnemy = null;
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, range, enemyMask);
+
+        if (enemies.Length <= 0) return null;
+        float minDistance = range;
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Enemy enemyChecked = enemies[i].GetComponent<Enemy>();
+
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyChecked.transform.position);
+
+            if (distanceToEnemy < minDistance)
+            {
+                closestEnemy = enemyChecked;
+                minDistance = distanceToEnemy;
+            }
+        }
+        return closestEnemy;
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, range);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(hitDetectionTransform.position, hitDetectionRadius);
+
+
+    }
+}
