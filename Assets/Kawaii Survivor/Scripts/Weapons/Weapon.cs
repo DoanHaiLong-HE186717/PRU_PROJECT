9b4d9434d3ab989c056774096fa6671360e2b88b
Long Doan <longdhhe186717@fpt.edu.vn>
2025-02-12 21:13:55 +0700

    object pooling range weapon bullet
    - apply object pooling to range weapon bullet

--- Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
+++ Assets/Kawaii Survivor/Scripts/Weapons/Weapon.cs
@@ -3,143 +3,37 @@ using System.Collections.Generic;
 using System.Net.Http.Headers;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
-    enum State
-    {
-        Idle,
-        Attack
-    }
-    private State state;
-
-    [Header("Elements")]
-    [SerializeField]
-    private Transform hitDetectionTransform;
-    [SerializeField]
-    private BoxCollider2D hitCollider;
-    [SerializeField]
-    private float hitDetectionRadius;
-
     [Header("Settings")]
     [SerializeField]
     private float range;
     [SerializeField]
-    private LayerMask enemyMask;
+    protected LayerMask enemyMask;
 
     [Header("Attack")]
     [SerializeField]
-    private int damage;
+    protected int damage;
     [SerializeField]
-    private float attackDelay;
+    protected float attackDelay;
     [SerializeField]
-    private Animator animator;
+    protected Animator animator;
 
-    private float attackTimer;
-    private List<Enemy> damagedEnemies = new List<Enemy>();
-
+    protected float attackTimer;
 
     [Header("Animation")]
     [SerializeField]
-    private float aimLerp;
+    protected float aimLerp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        state = State.Idle;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (state)
-        {
-            case State.Idle:
-                AutoAim();
-                break;
-            case State.Attack:
-                Attacking();
-                break;
-        }
-
-
     }
-    private void AutoAim()
-    {
-        Enemy closestEnemy = GetClosestEnemy();
-
-        Vector2 targetUpVector = Vector3.up;
-
-        if (closestEnemy != null)
-        {
-            targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
-            transform.up = targetUpVector;
-            ManageAttack();
-
-        }
-        transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
-        IncrementAttackTimer();
-    }
-
-    private void ManageAttack() 
-    {
-        if (attackTimer >= attackDelay) 
-        {
-            attackTimer = 0;
-            StartAttack();
-        }
-    }
-
-    private void IncrementAttackTimer() 
-    {
-        attackTimer += Time.deltaTime;
-    }
-
-    [NaughtyAttributes.Button]
-    private void StartAttack()
-    {
-        animator.Play("Attack");
-        state = State.Attack;
-        damagedEnemies.Clear();
-        animator.speed = 1f / attackDelay;
-    }
-    private void Attacking()
-    {
-        Attack();
-    }
-
-    private void StopAttack()
-    {
-        state = State.Idle;
-        // Clear the attacked enemies
-        //Damaged enemies list
-        damagedEnemies.Clear();
-    }
-    private void Attack()
-    {
-        //Collider2D[] enemies = Physics2D.OverlapCircleAll(hitDetectionTransform.position, hitDetectionRadius, enemyMask);
-        Collider2D[] enemies = Physics2D.OverlapBoxAll
-            (
-            hitDetectionTransform.position, 
-            hitCollider.bounds.size,
-            hitDetectionTransform.localEulerAngles.z,
-            enemyMask
-            );
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            Enemy enemy = enemies[i].GetComponent<Enemy>();
-            //1. Is the enemy inside of the list ?
-            if (!damagedEnemies.Contains(enemy)) 
-            {
-                enemy.TakeDamage(damage);
-                damagedEnemies.Add(enemy);
-            }
-
-            //2. If no let's attack him, and add it to the list
-
-            //3. If Yes, let's continue, check the next enemy
-        }
-    }
-    private Enemy GetClosestEnemy()
+    protected Enemy GetClosestEnemy()
     {
         Enemy closestEnemy = null;
 
@@ -168,10 +62,6 @@ public class Weapon : MonoBehaviour
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(transform.position, range);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(hitDetectionTransform.position, hitDetectionRadius);
-
 
     }
 }
