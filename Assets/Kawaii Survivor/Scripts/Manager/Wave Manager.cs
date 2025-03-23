7133032fc3bb62a869cf87a2e7493646d7bd0a3d
Hien <phanminhhien782004@gmail.com>
2025-03-23 14:13:03 +0700

    First Update on Inventory UI

--- Assets/Kawaii Survivor/Scripts/Manager/Wave Manager.cs
+++ Assets/Kawaii Survivor/Scripts/Manager/Wave Manager.cs
@@ -210,7 +210,7 @@ public class WaveManager : MonoBehaviour, IGameStateListener
     private void DefeatAllEnemies()
     {
         foreach (Enemy enemy in transform.GetComponentsInChildren<Enemy>())
-            enemy.PassAway();
+            enemy.PassAwayAfterWave();
     }
     
     private Vector2 GetSpawnPosition()
--- Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
+++ Assets/Kawaii Survivor/Scripts/Shop/ShopManagerUI.cs
@@ -1,16 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopManagerUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header(" Player Stats Elements ")]
+    [SerializeField] private GameObject playerStatsPanel;
+    [SerializeField] private GameObject playerStatsClosePanel;
+
+
+    [Header(" Inventory Elements ")]
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject inventoryClosePanel;
+
+    [Header(" Item Info Elements ")]
+    [SerializeField] private GameObject itemInfoSlidePanel;
+
+    [NaughtyAttributes.Button]
+    public void ShowPlayerStats()
     {
-        
+        playerStatsPanel.SetActive(true);
+        playerStatsClosePanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    [NaughtyAttributes.Button]
+    public void HidePlayerStats()
     {
-        
+        playerStatsPanel.SetActive(false);
+        playerStatsClosePanel.SetActive(false);
+
+    }
+
+    [NaughtyAttributes.Button]
+    public void ShowInventory()
+    {
+        inventoryPanel.SetActive(true);
+        inventoryClosePanel.SetActive(true);
+    }
+
+    [NaughtyAttributes.Button]
+    public void HideInventory(bool hideItemInfo = true)
+    {
+        inventoryPanel.SetActive(false);
+        inventoryClosePanel.SetActive(false);
+
+        if (hideItemInfo)
+            HideItemInfo();
+    }
+
+
+
+    [NaughtyAttributes.Button]
+    public void ShowItemInfo()
+    {
+        itemInfoSlidePanel.SetActive(true);
+
+    }
+
+    [NaughtyAttributes.Button]
+    public void HideItemInfo()
+    {
+        itemInfoSlidePanel.SetActive(false);
     }
 }
